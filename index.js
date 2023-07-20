a03fbf51b381260bed4f9cbd05c81e6f3b7fa610
davidurosa <urosaclawred21@gmail.com>
2023-07-20 13:08:39 -0400

    update 2

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var modal = document.getElementById("modal");
+
+
+
+// Obtiene el botón de cierre
+var span = document.getElementsByClassName("close")[0];
+
+
+// Cuando el usuario hace clic en el botón de cierre, cierra el modal
+span.onclick = function() {
+    console.log('hola');
+  modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic en cualquier lugar fuera del modal, cierra el modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 const emogis =["🤣","🤣","😍","😍","😒","😒","😎","😎","😜","😜","😆","😆","😢","😢","🤢","🤢"];
 var shuf_emagis = emogis.sort(()=> (Math.random() >.5) ? 2 : -1);
 for (let i = 0; i < emogis.length; i++) {
@@ -36,27 +59,4 @@ for (let i = 0; i < emogis.length; i++) {
 
 
 // Obtiene el modal
-var modal = document.getElementById("modal");
 
-// Obtiene el botón para abrir el modal
-var btn = document.getElementById("openModal");
-
-// Obtiene el botón de cierre
-var span = document.getElementsByClassName("close")[0];
-
-// Cuando el usuario hace clic en el botón, abre el modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// Cuando el usuario hace clic en el botón de cierre, cierra el modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Cuando el usuario hace clic en cualquier lugar fuera del modal, cierra el modal
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
